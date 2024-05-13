b46e36edd9152559f7d33db8919f2e5b8c0d239a
Osmani Cruz <osmanimorales16@gmail.com>
2024-05-13 10:42:34 -0600

    my_app

---
+++ src/data.js
@@ -0,0 +1,25 @@
+export const sculptureList = [{
+  name: 'un perrozuco',
+  artist: 'Facebook',
+  description: 'un ente recien descubierto.',
+  url: 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQO2YCeTqNaS5cBz8kZeACLc03lAjEvugCurpMWiNLBug&s',
+  alt: 'no se que poner.'  
+}, {
+  name: 'un gato',
+  artist: 'google',
+  description: 'Troste :(.',
+  url: 'https://imagenes.diariodenavarra.es/files/image_477_265/uploads/2021/02/18/60ae5c9db9f42.jpeg',
+  alt: 'no se que poner.'
+}, {
+  name: 'un carro',
+  artist: 'navegador',
+  description: 'kuchao.',
+  url: 'https://motor.elpais.com/wp-content/uploads/2024/03/Rayo-McQueen.jpg',
+  alt: 'no se que poner.'
+}, {
+  name: 'kirby?',
+  artist: 'super mario xd',
+  description: 'holi',
+  url: 'https://content.imageresizer.com/images/memes/Kirby-on-chair-meme-10.jpg',
+  alt: 'no se que poner.'
+}, ];
